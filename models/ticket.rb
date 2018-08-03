c7ad4aff8c90d714bc21603b31d1f73d04899129
Fiona Wilson <fionaw237@gmail.com>
2018-08-03 16:08:24 +0100

    Adds initializer

---
+++ models/ticket.rb
@@ -0,0 +1,13 @@
+require_relative("../db/sql_runner")
+
+class Ticket
+
+  attr_reader :id, :customer_id, :film_id
+
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @customer_id = options['customer_id']
+    @film_id = options['film_id']
+  end
+
+end
