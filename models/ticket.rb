d50629dad8b0d4709cda54ef2dd80ec4a3d79486
Fiona Wilson <fionaw237@gmail.com>
2018-08-03 16:51:06 +0100

    Adds update and delete methods

--- models/ticket.rb
+++ models/ticket.rb
@@ -2,7 +2,8 @@ require_relative("../db/sql_runner")
 
 class Ticket
 
-  attr_reader :id, :customer_id, :film_id
+  attr_reader :id
+  attr_accessor :customer_id, :film_id
 
   def initialize(options)
     @id = options['id'].to_i() if options['id']
@@ -21,6 +22,19 @@ class Ticket
     values = [@customer_id, @film_id]
     result = SqlRunner.run(sql, values).first()
     @id = result['id'].to_i()
+  end
+
+  def update()
+    sql = "UPDATE tickets SET
+           (customer_id, film_id) = ($1, $2) WHERE id = $3"
+    values = [@customer_id, @film_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM tickets WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
   end
 
 end
