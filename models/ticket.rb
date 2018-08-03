a182eee4f1d46838ac1b2b4904f543d66dae8de6
Fiona Wilson <fionaw237@gmail.com>
2018-08-03 16:41:58 +0100

    Adds save and delete_all methods

--- models/ticket.rb
+++ models/ticket.rb
@@ -10,4 +10,17 @@ class Ticket
     @film_id = options['film_id']
   end
 
+  def self.delete_all()
+    sql = "DELETE FROM tickets"
+    SqlRunner.run(sql)
+  end
+
+  def save()
+    sql = "INSERT INTO tickets (customer_id, film_id)
+           VALUES ($1, $2) RETURNING id"
+    values = [@customer_id, @film_id]
+    result = SqlRunner.run(sql, values).first()
+    @id = result['id'].to_i()
+  end
+
 end
