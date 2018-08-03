7d24e2a5fd90af25ceb3a5db407180e2fde61214
Fiona Wilson <fionaw237@gmail.com>
2018-08-03 16:16:44 +0100

    Adds delete_all and update methods

--- models/customer.rb
+++ models/customer.rb
@@ -11,12 +11,24 @@ class Customer
     @funds = options['funds']
   end
 
+  def self.delete_all()
+    sql = "DELETE FROM customers"
+    SqlRunner.run(sql)
+  end
+
   def save()
     sql = "INSERT INTO customers (name, funds)
            VALUES ($1, $2) RETURNING id"
     values = [@name, @funds]
     result = SqlRunner.run(sql, values).first()
     @id = result['id'].to_i()
+  end
+
+  def update()
+    sql = "UPDATE customers SET (name, funds) = ($1, $2)
+           WHERE id = $3"
+    values = [@name, @funds, @id]
+    SqlRunner.run(sql, values)
   end
 
 end
