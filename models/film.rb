2acc8a071f25169aed0323f2136458fa0e325b8d
Fiona Wilson <fionaw237@gmail.com>
2018-08-03 17:05:39 +0100

    Adds customers method

--- models/film.rb
+++ models/film.rb
@@ -37,4 +37,13 @@ class Film
     SqlRunner.run(sql, values)
   end
 
+  def customers()
+    sql = "SELECT customers.* FROM customers
+           INNER JOIN tickets ON tickets.customer_id = customers.id
+           WHERE tickets.film_id = $1"
+    values = [@id]
+    customers = SqlRunner.run(sql, values)
+    return customers.map {|customer| Customer.new(customer)}
+  end
+
 end
